b1e35cec31f00d7a60285a54f37b54cea182e915
joekotvas <joseph.kotvas@gmail.com>
2025-12-21 10:08:07 -0500

    refactor: extract interaction.ts into navigation modules

--- src/utils/navigation/index.ts
+++ src/utils/navigation/index.ts
@@ -6,8 +6,12 @@
  * @see previewNote.ts - Ghost cursor and preview note helpers
  * @see crossStaff.ts - Cross-staff navigation helpers
  * @see transposition.ts - Pitch transposition utilities
+ * @see horizontal.ts - Left/right keyboard navigation
+ * @see vertical.ts - Up/down keyboard navigation (chord/staff)
  */
 
 export * from './previewNote';
 export * from './crossStaff';
 export * from './transposition';
+export * from './horizontal';
+export * from './vertical';
